51b0e6f9afb90eaa24a789ed08859ebe174e09ce
Admin <tmstudentler@gmail.com>
2021-07-22 12:18:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/task5/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.task5;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager2 pager = (ViewPager2) findViewById(R.id.pager);
+        FragmentStateAdapter pageAdapter = new MyAdapter(this);
+        pager.setAdapter(pageAdapter);
+
+    }
+
+}
+
